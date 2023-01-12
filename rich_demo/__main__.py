a82ae842110a961edb21172fe609b637da040930
jmcortesa <jmcortesa@aragon.es>
2023-01-12 14:17:07 +0100

    Upgrade to v0.1.7

--- rich_demo/__main__.py
+++ rich_demo/__main__.py
@@ -1,8 +1,22 @@
-from rich.console import Console
 from rich.__main__ import make_test_card
+from rich.console import Console
+from rich.panel import Panel
+
+__version__ = "v0.1.7"
+
 
 def run():
-    Console().print(make_test_card())
+    console = Console()  # noqa
+    panel = Panel(
+        make_test_card(),
+        title=rf"[[b]Rich Demo[/b]] [gray30]{__version__}",
+        padding=(2, 0),
+        style="default on gray7",
+        width=120,
+        expand=False,
+    )
+    console.print(panel)
+
 
 if __name__ == "__main__":
     run()
